e2b6c78bc86c876d3b2480c4446afed721817d87
Kádár Ákos <akos@onyx.uvt.nl>
2022-01-17 09:53:48 +0100

    only encode pairwise distance

--- coref/pairwise_encoder.py
+++ coref/pairwise_encoder.py
@@ -74,3 +74,32 @@ class PairwiseEncoder(torch.nn.Module):
 
         # word id -> speaker id
         return [str2int[s] for s in doc["speaker"]]
+
+
+class DistancePairwiseEncoder(torch.nn.Module):
+    
+    def __init__(self, config: Config):
+        super().__init__()
+        emb_size = config.embedding_size
+        self.distance_emb = torch.nn.Embedding(9, emb_size)
+        self.dropout = torch.nn.Dropout(config.dropout_rate)
+        self.shape = emb_size
+
+    @property
+    def device(self) -> torch.device:
+        """ A workaround to get current device (which is assumed to be the
+        device of the first parameter of one of the submodules) """
+        return next(self.distance_emb.parameters()).device
+    
+
+    def forward(self,  # type: ignore  # pylint: disable=arguments-differ  #35566 in pytorch
+                top_indices: torch.Tensor,
+                doc: Doc) -> torch.Tensor:
+        word_ids = torch.arange(0, len(doc["cased_words"]), device=self.device)
+        distance = (word_ids.unsqueeze(1) - word_ids[top_indices]
+                    ).clamp_min_(min=1)
+        log_distance = distance.to(torch.float).log2().floor_()
+        log_distance = log_distance.clamp_max_(max=6).to(torch.long)
+        distance = torch.where(distance < 5, distance - 1, log_distance + 2)
+        distance = self.distance_emb(distance)
+        return self.dropout(distance)
