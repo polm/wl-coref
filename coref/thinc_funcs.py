979d0aef12d8ded0d260029cfbbd0ed540c44676
Kádár Ákos <akos@onyx.uvt.nl>
2022-02-16 17:54:55 +0100

    remove unused

--- coref/thinc_funcs.py
+++ coref/thinc_funcs.py
@@ -74,113 +74,6 @@ def spaCyRoBERTa(
     )
 
 
-def cluster_ids(
-) -> Model[const.Doc, Ints1d]:
-    """
-    Gets cluster_ids for each word in the Doc.
-    """
-    return Model(
-        'cluster_ids',
-        forward_cluster_ids,
-    )
-
-
-def doc2doc(
-) -> Model[const.Doc, spacy.tokens.Doc]:
-    """Convert Doc object from this library to spacy.tokens.Doc."""
-    return Model('doc2doc', forward_doc2doc)
-
-
-def doc2spaninfo(
-) -> Model[const.Doc, Tuple[Ints1d, Ints1d, Ints1d]]:
-    """Convert Doc to input to the SpanResolver model."""
-    return Model(
-        'prepare_for_spanresolver',
-        forward_doc2spaninfo
-    )
-
-
-def _sentids_to_sentstarts(
-    sent_ids: List[int]
-) -> List[int]:
-    """
-    Convert sentence id per token to sentence start indicators.
-    """
-    sent_starts = [1]
-    for i in range(1, len(sent_ids)):
-        start = int(sent_ids[i] != sent_ids[i - 1])
-        sent_starts.append(start)
-    return sent_starts
-
-
-def forward_cluster_ids(
-    model: Model[const.Doc, Ints1d],
-    doc: const.Doc,
-    is_train: bool
-) -> torch.Tensor:
-    word2cluster = {word_i: i
-                    for i, cluster in enumerate(doc["word_clusters"], start=1)
-                    for word_i in cluster}
-    clusters_list= [word2cluster.get(word_i, 0)
-                    for word_i in range(len(doc["cased_words"]))]
-
-    def backprop(dY):
-        return []
-
-    return model.ops.xp.asarray(clusters_list), backprop
-
-
-def forward_doc2doc(
-    model: Model[const.Doc, spacy.tokens.Doc],
-    doc: const.Doc,
-    is_train: bool
-) -> List[spacy.tokens.Doc]:
-    sent_starts = _sentids_to_sentstarts(doc['sent_id'])
-
-    def backprop(dY):
-        return []
-
-    spacy_doc = spacy.tokens.Doc(vocab=nlp.vocab,
-                                 words=doc['cased_words'],
-                                 sent_starts=sent_starts)
-    return [spacy_doc], backprop
-
-
-def forward_doc2spaninfo(
-    model: Model[const.Doc,
-                 Tuple[Ints1d, Ints1d, Ints1d]],
-    doc: const.Doc,
-    is_train: bool
-) -> Tuple[Ints1d, Ints1d, Ints1d]:
-    head2span = sorted(doc["head2span"])
-
-    def backprop(dY):
-        return []
-
-    if not head2span:
-
-        return (model.ops.xp.asarray([]),
-                model.ops.xp.asarray([]),
-                model.ops.xp.asarray([])
-                ), backprop
-
-    heads, starts, ends = zip(*head2span)
-    heads = model.ops.xp.asarray(heads)
-    starts = model.ops.xp.asarray(starts)
-    ends = model.ops.xp.asarray(ends) - 1
-
-
-    return (heads, starts, ends), backprop
-
-
-def doc2inputs(
-) -> Model[const.Doc, Tuple[Floats2d, Ints1d]]:
-    """
-#    Create pipeline that goes from Doc to RoBERTa
-#    features and cluster_ids.
-#    """
-    encoder = chain(doc2doc(), spaCyRoBERTa())
-    return tuplify(encoder, cluster_ids())
 
 
 def doc2tensors(
@@ -228,8 +121,6 @@ def configure_pytorch_modules(config):
         ),
         convert_inputs=convert_span_predictor_inputs
     )
-    coref_scorer.initialize()
-    span_predictor.initialize()
     return coref_scorer, span_predictor
 
 
